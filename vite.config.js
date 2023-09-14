8d1ca15610cb5eb9407e115a079ecfdae1430fb5
YeongZhun <121158803+YeongZhun@users.noreply.github.com>
2023-09-14 10:29:21 +0800

    back to normal first

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import dotenv from 'dotenv'
-
-// dotenv.config() // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // 'process.env': process.env,
-    // MONGODB_URI: process.env.MONGODB_URI,
-
-    // 'VITE_BACKEND_URL': process.env.VITE_BACKEND_URL,
-    // __VALUE__: `"${process.env.VALUE}"` // wrapping in "" since it's a string
-  }
 })
-
